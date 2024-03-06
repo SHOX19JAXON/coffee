bad7809256e9233146074cf27598422e59c7f5f5
Shoxjaxon <mshoxjaxon19@gmail.com>
2024-03-06 17:38:06 +0500

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,42 @@
+import 'package:coffee/utils/theme/app_theme.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:adaptive_theme/adaptive_theme.dart';
+import 'home_screen.dart';
+
+
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ScreenUtilInit(
+        designSize: const Size(360, 800),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (_ , child) {
+
+          return AdaptiveTheme(
+            light: AppTheme.lightTheme,
+            dark: AppTheme.darkTheme,
+            initial: AdaptiveThemeMode.system,
+            builder: (theme, darkTheme) {
+              return MaterialApp(
+                debugShowCheckedModeBanner: false,
+                theme: theme,
+                darkTheme: darkTheme,
+                home: HomeScreen(),
+              );
+            },
+          );
+        },
+        child:const HomeScreen()
+
+    );
+  }
+}
---
+++ lib/utils/colors/app_colors.dart
@@ -0,0 +1,18 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color black = Colors.black;
+  static const Color white = Colors.white;
+  static const Color transparent = Colors.transparent;
+
+  static const Color c_1A72DD = Color(0xFF1A72DD);
+  static const Color c_F4261A = Color(0xFFF4261A);
+  static const Color c_2A3256 = Color(0xFF2A3256);
+  static const Color c_C4C4C4= Color(0xFFC4C4C4);
+  static const Color c_273032= Color(0xFF273032);
+  static const Color c_409DB9= Color(0xFF409DB9);
+  static const Color c_090F47= Color(0xFF090F47);
+  static const Color c_072832= Color(0xFF072832);
+}
